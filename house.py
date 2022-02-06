31eca535145f4598774a7c326b1e5ef2ec69c5d6
DaniyarZh <den-gak@mail.ru>
2022-02-06 15:04:56 +0400

    Сделал декомпозицию функции рисования домика.
    Создана функция рисования домика, как заготовка (прототип)
    и документирован её интерфейс.
    Если добавить её реализацию, то программа заработает.

--- house.py
+++ house.py
@@ -1,3 +1,8 @@
+def main():
+    x, y = 100, 100
+    width, height = 200, 200
+    draw_house(x, y, width, height)
+
 
 def draw_house(x, y, width, height):
     '''
@@ -9,10 +14,40 @@ def draw_house(x, y, width, height):
     : param height: полная высота домика
     : return: None
     '''
+    foundation_height = 0.05 * height  # высота фундамента 5% от всей
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
     print(x, y, width, height)
     pass
 
-x, y = 100, 100
-width, height = 200, 200
 
-draw_house(x, y, width, height)
+def draw_house_foundation(x, y, width, height):
+    '''
+    Нарисовать основание ширины  домика width и высоты height от опорной точки(x, y),
+    которая находится в середине нижней точки фундамента.
+    : param x: координата x середины фундамента
+    : param y: координата y низа фундамента
+    : param width: полная ширина фундамента
+    : param foundation_height: полная высота фундамента
+    : return: None
+    '''
+    pass
+    print('Типа рисую фундамент', x, y, width, height)
+
+
+def draw_house_walls(x, y, width, height):
+    pass
+    print('Типа рисую стены', x, y, width, height)
+
+
+def draw_house_roof(x, y, width, height):
+    pass
+    print('Типа рисую крышу', x, y, width, height)
+
+
+main()
